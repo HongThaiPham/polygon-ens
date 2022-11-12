d99523bfe0c98528ab9249badb5019654bc98b44
leopham <hongthaipro@gmail.com>
2022-11-12 15:22:19 +0700

    init contract

---
+++ contracts/Domains.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+        console.log("domain contract");
+    }
+}
---
+++ scripts/run.ts
@@ -0,0 +1,20 @@
+import * as hre from "hardhat";
+
+const main = async () => {
+  const domainContractFactory = await hre.ethers.getContractFactory("Domains");
+  const domainContract = await domainContractFactory.deploy();
+  await domainContract.deployed();
+  console.log("Contract deployed to:", domainContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
