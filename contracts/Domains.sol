ce085b28c04ffd15cf3175ca9cd503c15a8251a9
leopham <hongthaipro@gmail.com>
2022-11-12 15:35:16 +0700

    save domains

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,7 +5,18 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract Domains {
+    mapping(string => address) public domains;
+
     constructor() {
         console.log("domain contract");
+    }
+
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
     }
 }
--- scripts/run.ts
+++ scripts/run.ts
@@ -1,10 +1,18 @@
 import * as hre from "hardhat";
 
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  const txn = await domainContract.register("doom");
+  await txn.wait();
+
+  const domainOwner = await domainContract.getAddress("doom");
+  console.log("Owner of domain:", domainOwner);
 };
 
 const runMain = async () => {
