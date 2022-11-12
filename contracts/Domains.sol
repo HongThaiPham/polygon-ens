a2344538f2b63af96e4222187e402416d1f9ed04
leopham <hongthaipro@gmail.com>
2022-11-12 16:30:56 +0700

    Getting paid and going

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,19 +1,39 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
-
+import {StringUtils} from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+    string public tld;
     mapping(string => address) public domains;
     mapping(string => string) public records;
 
-    constructor() {
-        console.log("domain contract");
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("%s name service deployed", _tld);
     }
 
-    function register(string calldata name) public {
+    function price(string calldata name) public pure returns (uint256) {
+        uint256 len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+            return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+        } else if (len == 4) {
+            return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+        } else {
+            return 1 * 10**17;
+        }
+    }
+
+    function register(string calldata name) public payable {
         require(domains[name] == address(0));
+
+        uint256 _price = price(name);
+
+        // Check if enough Matic was paid in the transaction
+        require(msg.value >= _price, "Not enough Matic paid");
+
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
